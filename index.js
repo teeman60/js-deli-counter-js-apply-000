f72144482370acd40d1d60ff44810df21dec9a75
Taiye Salami <taiye41@gmail.com>
2019-07-12 07:03:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeliLine = [];
 
  function currentLine(x) {
     var line = []
-    if (x.length === 0) {
+    if (katzDeliLine.length === 0) {
       return "The line is currently empty."
     } else {
       for(var i = 0; i < x.length; i++) {
